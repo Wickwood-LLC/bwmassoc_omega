9abaf4aaddf28ba5d83afedce05ec30864be8a96
palagpatski <palagpatski@gmail.com>
2016-12-06 16:23:58 +0800

    Group title and primary tabs in a title bar

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -101,13 +101,15 @@
       <?php print render($page['highlighted']); ?>
       <?php print $breadcrumb; ?>
       <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1 class="page-title"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
+      <div class="title-bar">
+        <?php print render($title_prefix); ?>
+        <?php if ($title): ?>
+          <h1 class="page-title"><?php print $title; ?></h1>
+        <?php endif; ?>
+        <?php print render($title_suffix); ?>
+        <?php print render($tabs); ?>
+      </div>
       <?php print $messages; ?>
-      <?php print render($tabs); ?>
       <?php print render($page['help']); ?>
       <?php if ($action_links): ?>
         <ul class="action-links"><?php print render($action_links); ?></ul>
