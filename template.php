e29a13029a4532de6e187fcb3200fcdd595bc2fa
palagpatski <palagpatski@gmail.com>
2017-01-17 21:13:40 +0800

    Place secondary menu under the primary tabs

--- template.php
+++ template.php
@@ -49,19 +49,20 @@ function bwmassoc_omega_menu_local_tasks(&$variables) {
 
   dpm($variables);
 
+  if (!empty($variables['secondary'])) {
+	$variables['secondary']['#prefix'] = '<h2 class="element-invisible">' . t('Secondary tabs') . '</h2>';
+	$variables['secondary']['#prefix'] .= '<ul class="tabs tabs--secondary links--inline">';
+	$variables['secondary']['#suffix'] = '</ul>';
+  }
+
   if (!empty($variables['primary'])) {
     $variables['primary']['#prefix'] = '<h2 class="element-invisible">' . t('Primary tabs') . '</h2>';
     $variables['primary']['#prefix'] .= '<ul class="tabs tabs--primary  links--inline">';
     $variables['primary']['#suffix'] = '</ul>';
-    $variables['primary']['children'] = 'test';
-
-	if (!empty($variables['secondary'])) {
-		$variables['secondary']['#prefix'] = '<h2 class="element-invisible">' . t('Secondary tabs') . '</h2>';
-		$variables['secondary']['#prefix'] .= '<ul class="tabs tabs--secondary links--inline">';
-		$variables['secondary']['#suffix'] = '</ul>';
-
-		$variables['primary']['children'] = $variables['secondary'];
-	}
+    
+    if (!empty($variables['secondary'])) {
+    	$variables['primary']['children'] = $variables['secondary'];
+    }
 
     $output .= drupal_render($variables['primary']);
   }
