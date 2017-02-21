171eb6ebb78017adb586d6c0699d2a3715e6a574
palagpatski <palagpatski@gmail.com>
2017-02-21 09:40:58 +0800

    Fix viewport meta to get superfish smallscreen plugin to work on mobile devices

--- template.php
+++ template.php
@@ -146,3 +146,14 @@ function bwmassoc_omega_field_multiple_value_form($variables) {
   return $output;
 }
 
+function bwmassoc_omega_preprocess_html(&$vars) {
+  $viewport = array(
+   '#tag' => 'meta',
+   '#attributes' => array(
+     'name' => 'viewport',
+     'content' => 'width=device-width, initial-scale=1, user-scalable=yes',
+   ),
+  );
+  drupal_add_html_head($viewport, 'viewport');
+}
+
