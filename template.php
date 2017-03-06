0a5e0c9a3a640ff1e5e5d87f5997517fdd0ccd36
palagpatski <palagpatski@gmail.com>
2017-03-06 17:52:34 +0800

    Load jquery mobile via https

--- template.php
+++ template.php
@@ -8,10 +8,10 @@
 
 function bwmassoc_omega_preprocess_html(&$vars) {
   // Load font
-  drupal_add_css('http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css', array(
+  drupal_add_css('https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css', array(
     'type' => 'external'
   ));
-  drupal_add_js('http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js', array(
+  drupal_add_js('https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js', array(
     'type' => 'external'
   ));
 }
