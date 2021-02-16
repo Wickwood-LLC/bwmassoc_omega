4f88804918311ed7344891fc75928340b5faefdc
palagpatski <palagpatski@gmail.com>
2021-02-16 19:31:29 +0800

    Adding favicons

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -59,21 +59,21 @@
   <?php print $styles; ?>
   <?php print $scripts; ?>
   <link rel="apple-touch-icon" sizes="57x57" href="/<?php print drupal_get_path('theme',$GLOBALS['theme']); ?>/favicons/apple-icon-57x57.png">
-  <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
-  <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
-  <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
-  <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
-  <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
-  <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
-  <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
-  <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
-  <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-  <link rel="manifest" href="/manifest.json">
+  <link rel="apple-touch-icon" sizes="60x60" href="/<?php print drupal_get_path('theme',$GLOBALS['theme']); ?>/favicons/apple-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="/<?php print drupal_get_path('theme',$GLOBALS['theme']); ?>/favicons/apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="/<?php print drupal_get_path('theme',$GLOBALS['theme']); ?>/favicons/apple-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="/<?php print drupal_get_path('theme',$GLOBALS['theme']); ?>/favicons/apple-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="/<?php print drupal_get_path('theme',$GLOBALS['theme']); ?>/favicons/apple-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="/<?php print drupal_get_path('theme',$GLOBALS['theme']); ?>/favicons/apple-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="/<?php print drupal_get_path('theme',$GLOBALS['theme']); ?>/favicons/apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="/<?php print drupal_get_path('theme',$GLOBALS['theme']); ?>/favicons/apple-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="192x192"  href="/<?php print drupal_get_path('theme',$GLOBALS['theme']); ?>/favicons/android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/<?php print drupal_get_path('theme',$GLOBALS['theme']); ?>/favicons/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="/<?php print drupal_get_path('theme',$GLOBALS['theme']); ?>/favicons/favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/<?php print drupal_get_path('theme',$GLOBALS['theme']); ?>/favicons/favicon-16x16.png">
+  <link rel="manifest" href="/<?php print drupal_get_path('theme',$GLOBALS['theme']); ?>/favicons/manifest.json">
   <meta name="msapplication-TileColor" content="#ffffff">
-  <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+  <meta name="msapplication-TileImage" content="/<?php print drupal_get_path('theme',$GLOBALS['theme']); ?>/favicons/ms-icon-144x144.png">
   <meta name="theme-color" content="#ffffff">
 </head>
 <body<?php print $attributes;?>>
